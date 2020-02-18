2238d4262b3668196d79f6fc909455ada7ab3873
Nemhis <p.nemphis@gmail.com>
2020-02-18 22:10:33 +0300

    Adding prop-types

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import PropTypes from "prop-types";
+
 import WelcomeScreen from '../welcome-screen/welcome-screen';
 
-
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {name} = props;
 
   return <React.Fragment>
     <WelcomeScreen name={name} />
   </React.Fragment>;
+};
+
+App.propTypes = {
+  name: PropTypes.number.isRequired,
 };
 
 export default App;
--- src/components/welcome-screen/welcome-screen.js
+++ src/components/welcome-screen/welcome-screen.js
@@ -1,12 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const WelcomeScreen = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {name = `Stranger`} = props;
 
   return <React.Fragment>
     <p>Hello, {name}.</p>
   </React.Fragment>;
+};
+
+WelcomeScreen.propTypes = {
+  name: PropTypes.number.isRequired,
 };
 
 export default WelcomeScreen;
