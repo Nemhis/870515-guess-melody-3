39ce3e0e5bddd095036c06280d753f620a31ca3e
Nemhis <p.nemphis@gmail.com>
2020-02-19 21:35:08 +0300

    Formatting return component

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -4,12 +4,14 @@ import PropTypes from 'prop-types';
 const WelcomeScreen = (props) => {
   const {name = `Stranger`, onWelcomeButtonClick} = props;
 
-  return <React.Fragment>
-    <p>Hello, {name}.</p>
-    <button className="welcome__button" onClick={onWelcomeButtonClick}>
-      <span className="visually-hidden">Начать игру</span>
-    </button>
-  </React.Fragment>;
+  return (
+    <React.Fragment>
+      <p>Hello, {name}.</p>
+      <button className="welcome__button" onClick={onWelcomeButtonClick}>
+        <span className="visually-hidden">Начать игру</span>
+      </button>
+    </React.Fragment>
+  );
 };
 
 WelcomeScreen.propTypes = {
