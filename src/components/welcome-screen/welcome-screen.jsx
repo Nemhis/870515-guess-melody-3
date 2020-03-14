1f61c274383dabdbab518469a0db15f44021df2c
Nemhis <p.nemphis@gmail.com>
2020-03-14 16:31:58 +0300

    Update WelcomeScreen markup

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -5,11 +5,22 @@ const WelcomeScreen = (props) => {
   const {onWelcomeButtonClick} = props;
 
   return (
-    <React.Fragment>
+    <section className="welcome">
+      <div className="welcome__logo">
+        <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
+      </div>
       <button className="welcome__button" onClick={onWelcomeButtonClick}>
         <span className="visually-hidden">Начать игру</span>
       </button>
-    </React.Fragment>
+
+      <h2 className="welcome__rules-title">Правила игры</h2>
+      <p className="welcome__text">Правила просты:</p>
+      <ul className="welcome__rules-list">
+        <li>Нужно ответить на все вопросы.</li>
+        <li>Можно допустить 3 ошибки.</li>
+      </ul>
+      <p className="welcome__text">Удачи!</p>
+    </section>
   );
 };
 
