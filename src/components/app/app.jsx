7aea8a7e940d0cf1859bf562275c5d3cb76dac2f
Nemhis <p.nemphis@gmail.com>
2020-03-14 14:34:24 +0300

    Fix style definition

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen.jsx';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen.jsx';
 
 const App = (props) => {
   const {name} = props;
 
-  return <WelcomeScreen name={name} onWelcomeButtonClick={() => {}}/>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen name={name} onWelcomeButtonClick={() => {}}/>
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
